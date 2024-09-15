b4a83f95bda9ac3ec87a7614d41fdeff42d9d015
Suntein <3147539548@qq.com>
2024-09-16 00:15:08 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    cout << "hello world!!" << endl ; //prints "hello world!!" endl 可以被替换为 "\n"
+    printf("helloworld");//printf包含于iostream
+    return 0 ;
+}
